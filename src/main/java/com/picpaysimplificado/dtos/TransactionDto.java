048586af6d0b7673819c52802d62b2e113387a37
Douglas Alves Marcelino <douglasmarcelinodev@gmail.com>
2024-06-26 02:40:45 -0300

    feat: create TransactionDto

---
+++ src/main/java/com/picpaysimplificado/dtos/TransactionDto.java
@@ -0,0 +1,10 @@
+package com.picpaysimplificado.dtos;
+
+import java.math.BigDecimal;
+
+public record TransactionDto(
+        BigDecimal value,
+        Long senderId,
+        Long receiverId
+) {
+}
