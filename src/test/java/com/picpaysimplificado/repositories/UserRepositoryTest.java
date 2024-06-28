598663eaae49177fec62b89d3218f03d3e2c1dc9
Douglas Alves Marcelino <douglasmarcelinodev@gmail.com>
2024-06-28 02:30:16 -0300

    feat: add unit tests to UserRepository

---
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDto;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("Should get User successfully from DB")
+    void findUserByDocumentCase1() {
+        String document = "99999999901";
+        var data = new UserDto("Douglas", "Marcelino", document, new BigDecimal(10),
+                "doug@gmail.com", "12345", UserType.COMMON);
+        this.createUser(data);
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should not get User from DB when user not exists")
+    void findUserByDocumentCase2() {
+        String document = "99999999901";
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private User createUser(UserDto data) {
+        var newUser = new User(data);
+        this.entityManager.persist(newUser);
+        return newUser;
+    }
+}
